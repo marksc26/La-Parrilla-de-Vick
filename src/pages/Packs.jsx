ec757abda57091dee6a4b4b5f92660d2f24bb436
*Marcos <maromasi@hotmail.com>
2023-06-20 23:53:20 -0600

    2nd commit

--- src/pages/Packs.jsx
+++ src/pages/Packs.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './styles/Packs.css'
-import { packs } from '../../public/utils/data.json'
+import { packs } from '../assets/utils/data.json'
 
 const Packs = () => {
 
